b5137ab2c8aa47c50f367579ab0a8a955025e3ea
F4kogLc <77386323+F4kogLc@users.noreply.github.com>
2021-02-01 10:44:00 +0500

    Create AntiAim.lua

---
+++ moonloader/AntiAim.lua
@@ -0,0 +1,21 @@
+require 'lib.moonloader'
+local SE = require 'lib.samp.events'
+
+local AntiAim = false
+
+function main()
+    repeat wait(0) until isSampAvailable()
+    wait(0)
+    sampRegisterChatCommand('aa', function()
+        AntiAim = not AntiAim
+    end)
+end
+
+function SE.onSendPlayerSync(data)
+    if AntiAim then
+        data.quaternion[0] = math.random(-1,1)
+        data.quaternion[1] = math.random(-1,1)
+        data.quaternion[2] = math.random(-1,1)
+        data.quaternion[3] = math.random(-1,1)
+    end
+end
