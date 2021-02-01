2090cd811e54c5bd8e5651e6ba4e172bafc0b447
F4kogLc <77386323+F4kogLc@users.noreply.github.com>
2021-02-01 12:21:58 +0500

    Update Fixes.lua

--- moonloader/Fixes.lua
+++ moonloader/Fixes.lua
@@ -1,4 +1,7 @@
+require('memory').setuint32((0x0052228C + 0x2), 0xB6EC1C) -- ptr to vertical mouse acceleration -> ptr to horizontal
 require('memory').fill(0x00531155, 0x90, 5, true) -- shift afk fix
 require('memory').fill(sampGetBase() + 0x2D3C45, 0, 2, true) -- fast connect
-writeMemory(0x555854, 4, -1869574000, true) -- fix bug
-writeMemory(0x555858, 1, 144, true) -- fix bug
+require('memory').fill(0x555854, 0x90, 5, true) -- fix interior
+-- fix interior
+--writeMemory(0x555854, 4, -1869574000, true)
+--writeMemory(0x555858, 1, 144, true)
